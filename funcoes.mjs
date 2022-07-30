923fa5f80b2c76ea205b7bca30a2523fe7a237c8
Amanda Ribeiro <amanda_ribeiro98@yahoo.com.br>
2022-07-30 19:02:39 -0300

    Add files via upload

---
+++ funcoes.mjs
@@ -0,0 +1,13 @@
+function mostraIdade(nome, idade) {
+    return 'A idade de ${nome} e ${idade}';
+}
+
+function mostraCidade(nome, cidade) {
+    return 'A cidade de ${nome} e ${cidade}';
+}
+
+function mostraHobby(nome, Hobby) {
+    return 'O Hobby de ${nome} e ${Hobby}';
+}
+
+export{ mostraIdade, mostraCidade, mostraHobby } ;
---
+++ main.js
@@ -0,0 +1,6 @@
+import { mostraCidade, mostraIdade, mostraHobby 
+} from "./funcoes.mjs";
+
+console.log(mostraIdade('Amanda', 25));
+console.log(mostraCidade('Amanda', 'São Paulo'));
+console.log(mostraHobby('Amanda', 'ler livros'));
